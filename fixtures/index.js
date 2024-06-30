4f85deaa4defdaf91e310cf3d5d970dd7fdb7261
ScriptedAlchemy <zackaryjackson@bytedance.com>
2024-06-29 21:32:39 -0700

    feat: memory fs

--- fixtures/index.js
+++ fixtures/index.js
@@ -1,4 +1,4 @@
 import { answer } from './answer.js';
 import hello from "data:text/javascript,module.exports='Hello, World 1234!'";
-import React from "https://esm.sh/react@18.2.0";
+import React from "https://esm.sh/react-dom@18.2.0";
 console.log('answer:',answer,hello, React);
